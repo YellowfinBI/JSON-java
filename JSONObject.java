0323ff478d49e121502a7b8d6d6be39a12e02b7b
Steve Joynt <steve.joynt@yellowfin.bi>
2015-05-27 11:10:36 +1000

    Added a separate constructor for to allow null values to be included when creating a JSONObject from an Object. Restored the default behaviour for the Object and Map constructors.

--- JSONObject.java
+++ JSONObject.java
@@ -245,7 +245,7 @@ public class JSONObject {
      * @throws JSONException
      */
     public JSONObject(Map<?,?> map) {
-       this(map, true);
+       this(map, false);
     }
 
     /**
@@ -295,8 +295,36 @@ public class JSONObject {
      *            a JSONObject.
      */
     public JSONObject(Object bean) {
+        this(bean, false);
+    }
+
+    /**
+     * Construct a JSONObject from an Object using bean getters. It reflects on
+     * all of the public methods of the object. For each of the methods with no
+     * parameters and a name starting with <code>"get"</code> or
+     * <code>"is"</code> followed by an uppercase letter, the method is invoked,
+     * and a key and the value returned from the getter method are put into the
+     * new JSONObject.
+     *
+     * The key is formed by removing the <code>"get"</code> or <code>"is"</code>
+     * prefix. If the second remaining character is not upper case, then the
+     * first character is converted to lower case.
+     *
+     * For example, if an object has a method named <code>"getName"</code>, and
+     * if the result of calling <code>object.getName()</code> is
+     * <code>"Larry Fine"</code>, then the JSONObject will contain
+     * <code>"name": "Larry Fine"</code>.
+     *
+     * @param bean
+     *            An object that has getter methods that should be used to make
+     *            a JSONObject.
+     * @param includeNullValues
+     *            If set to false, any properties in the bean that have null values
+     *            will not have corresponding keys created in the JSONObject.
+     */
+    public JSONObject(Object bean, boolean includeNullValues) {
         this();
-        this.populateMap(bean);
+        this.populateMap(bean, includeNullValues);
     }
 
     /**
@@ -997,7 +1025,7 @@ public class JSONObject {
         return NULL.equals(object) ? defaultValue : object.toString();
     }
 
-    private void populateMap(Object bean) {
+    private void populateMap(Object bean, boolean includeNullValues) {
         Class<?> klass = bean.getClass();
 
 // If klass is a System class then set includeSuperClass to false.
@@ -1034,7 +1062,9 @@ public class JSONObject {
 
                         if (!method.getReturnType().equals(Void.TYPE)) {
                            Object result = method.invoke(bean, (Object[]) null);
-                           this.map.put(key, wrap(result));
+                           if (result != null || includeNullValues) {
+                              this.map.put(key, wrap(result));
+                           }
                         }
                     }
                 }
